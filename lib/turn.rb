9941be5e37a7ee08434b027070f08b5d4a719c81
Philip Warren <philwarren1@gmail.com>
2020-07-12 15:41:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,8 +22,8 @@ def valid_move?(board, index)
       return false
    end
 end
-def move(board, index, player)
-   board[index] = player
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
 def turn(board)
   puts "Please enter 1-9:"
